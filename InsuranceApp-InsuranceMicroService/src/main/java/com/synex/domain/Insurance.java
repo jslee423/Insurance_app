b5ec547d0886a5c557d75a1eada8362d642fdc16
Jason Lee <jslee423@gmail.com>
2024-02-14 18:15:53 -0500

    add completed insurance and policy entities

--- InsuranceApp-InsuranceMicroService/src/main/java/com/synex/domain/Insurance.java
+++ InsuranceApp-InsuranceMicroService/src/main/java/com/synex/domain/Insurance.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -15,7 +17,62 @@ public class Insurance {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String name;
+	@Enumerated(EnumType.STRING)
 	private InsuranceType insuranceType;
+	@Enumerated(EnumType.STRING)
+	private PlanType planType;
 	@OneToMany
 	private List<Plan> plans = new ArrayList<>();
+	
+	public Insurance() {}
+
+	public Insurance(Long id, String name, InsuranceType insuranceType, PlanType planType, List<Plan> plans) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.insuranceType = insuranceType;
+		this.planType = planType;
+		this.plans = plans;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public InsuranceType getInsuranceType() {
+		return insuranceType;
+	}
+
+	public void setInsuranceType(InsuranceType insuranceType) {
+		this.insuranceType = insuranceType;
+	}
+
+	public PlanType getPlanType() {
+		return planType;
+	}
+
+	public void setPlanType(PlanType planType) {
+		this.planType = planType;
+	}
+
+	public List<Plan> getPlans() {
+		return plans;
+	}
+
+	public void setPlans(List<Plan> plans) {
+		this.plans = plans;
+	}
 }
--- InsuranceApp-InsuranceMicroService/src/main/java/com/synex/domain/Policy.java
+++ InsuranceApp-InsuranceMicroService/src/main/java/com/synex/domain/Policy.java
@@ -4,10 +4,13 @@ import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -25,6 +28,86 @@ public class Policy {
 	private LocalDate endDate;
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
 	private LocalDate renewDate;
+	@ManyToOne
 	private Plan plan;
 	private Double price;
+	
+	public Policy() {}
+
+	public Policy(Long id, Customer customer, String status, LocalDate openDate, LocalDate endDate, LocalDate renewDate,
+			Plan plan, Double price) {
+		super();
+		this.id = id;
+		this.customer = customer;
+		this.status = status;
+		this.openDate = openDate;
+		this.endDate = endDate;
+		this.renewDate = renewDate;
+		this.plan = plan;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDate getOpenDate() {
+		return openDate;
+	}
+
+	public void setOpenDate(LocalDate openDate) {
+		this.openDate = openDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public LocalDate getRenewDate() {
+		return renewDate;
+	}
+
+	public void setRenewDate(LocalDate renewDate) {
+		this.renewDate = renewDate;
+	}
+
+	public Plan getPlan() {
+		return plan;
+	}
+
+	public void setPlan(Plan plan) {
+		this.plan = plan;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 }
