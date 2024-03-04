3e8299dd1de37fd683da82f9a2f12898881585da
Jason Lee <jslee423@gmail.com>
2024-03-04 13:33:02 -0500

    add remaining api calls to insurance microservice

--- InsuranceApp-client/src/main/java/com/synex/client/InsuranceClient.java
+++ InsuranceApp-client/src/main/java/com/synex/client/InsuranceClient.java
@@ -13,6 +13,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Component
 public class InsuranceClient {
 
+	//----------------- INSURANCE -----------------
 	public JsonNode saveInsurance(JsonNode json) {
 		System.out.println("InsuranceClient.saveInsurance()...");
 		
@@ -41,6 +42,7 @@ public class InsuranceClient {
 		return returnObj;
 	}
 	
+	//----------------- PLANS -----------------
 	public JsonNode findPlanByInsuranceId(Long insuranceId) {
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("http://localhost:8484/findPlanByInsuranceId/"+insuranceId, Object.class);
@@ -74,6 +76,24 @@ public class InsuranceClient {
 		return returnObj;
 	}
 	
+	public JsonNode getPlanQuote(JsonNode json) {
+		System.out.println("InsuranceClient.getPlanQuote()...");
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<String> request = new HttpEntity<String>(json.toString(), headers);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.postForEntity("http://localhost:8484/getPlanQuote", request, Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	} 
+	
+	//----------------- CUSTOMER -----------------
 	public JsonNode saveCustomer(JsonNode json) {
 		System.out.println("InsuranceClient.saveCustomer()...");
 		
@@ -91,15 +111,123 @@ public class InsuranceClient {
 		return returnObj;
 	} 
 	
-	public JsonNode getPlanQuote(JsonNode json) {
-		System.out.println("InsuranceClient.getPlanQuote()...");
+	public JsonNode findCustomerByUserId(Long userId) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("http://localhost:8484/findCustomerByUserId/"+userId, Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	}
+	
+	public JsonNode addCustomerDocs(Long customerId, String addressDoc, String govIdDoc) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("http://localhost:8484/addCustomerDocs/"+customerId+"/"+addressDoc+"/"+govIdDoc, Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	} 
+	
+	//----------------- POLICY -----------------
+	public JsonNode savePolicy(JsonNode json) {
+		System.out.println("InsuranceClient.savePolicy()...");
 		
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<String> request = new HttpEntity<String>(json.toString(), headers);
 		
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Object> responseEntity = restTemplate.postForEntity("http://localhost:8484/getPlanQuote", request, Object.class);
+		ResponseEntity<Object> responseEntity = restTemplate.postForEntity("http://localhost:8484/savePolicy", request, Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	} 
+	
+	public JsonNode findPolicyById(Long policyId) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("http://localhost:8484/findPolicyById/"+policyId, Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	}
+	
+	public JsonNode findAllPolicies() {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("http://localhost:8484/findAllPolicies", Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	}
+	
+	public JsonNode findPolicyByCustomerId(Long customerId) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("http://localhost:8484/findPolicyByCustomerId/"+customerId, Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	}
+	
+	public JsonNode findPolicyByStatus(String status) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("http://localhost:8484/findPolicyByStatus/"+status, Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	}
+	
+	public JsonNode approvePolicy(Long policyId) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("http://localhost:8484/approvePolicy/"+policyId, Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	} 
+	
+	public JsonNode denyPolicy(Long policyId) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity("http://localhost:8484/denyPolicy/"+policyId, Object.class);
+		Object objects = responseEntity.getBody();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode returnObj = mapper.convertValue(objects, JsonNode.class);
+		
+		return returnObj;
+	} 
+	
+	
+	//----------------- PAYMENT -----------------
+	public JsonNode savePayment(JsonNode json) {
+		System.out.println("InsuranceClient.savePolicy()...");
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<String> request = new HttpEntity<String>(json.toString(), headers);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Object> responseEntity = restTemplate.postForEntity("http://localhost:8484/savePayment", request, Object.class);
 		Object objects = responseEntity.getBody();
 		
 		ObjectMapper mapper = new ObjectMapper();
--- InsuranceApp-client/src/main/java/com/synex/controller/InsuranceController.java
+++ InsuranceApp-client/src/main/java/com/synex/controller/InsuranceController.java
@@ -1,13 +1,11 @@
 package com.synex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.synex.client.InsuranceClient;
@@ -17,6 +15,7 @@ public class InsuranceController {
 	
 	@Autowired InsuranceClient insuranceClient;
 
+	//----------------- INSURANCE -----------------
 	@GetMapping("findAllInsurance")
 	public JsonNode findAllInsurance() {
 		insuranceClient.findAllInsurance().forEach(insurance -> {
@@ -25,6 +24,7 @@ public class InsuranceController {
 		return insuranceClient.findAllInsurance();
 	}
 	
+	//----------------- PLAN -----------------
 	@GetMapping("findPlanByInsuranceId/{insuranceId}")
 	public JsonNode findPlanByInsuranceId(@PathVariable Long insuranceId) {
 		return insuranceClient.findPlanByInsuranceId(insuranceId);
@@ -35,13 +35,61 @@ public class InsuranceController {
 		return insuranceClient.findAllPlans();
 	} 
 	
+	@PostMapping("getPlanQuote")
+	public JsonNode getPlanQuote(@RequestBody JsonNode jsonNode) {
+		return insuranceClient.getPlanQuote(jsonNode);
+	}
+	
+	//----------------- CUSTOMER -----------------
 	@PostMapping("saveCustomer")
 	public JsonNode saveCustomer(@RequestBody JsonNode jsonNode) {
 		return insuranceClient.saveCustomer(jsonNode);
 	}
 	
-	@PostMapping("getPlanQuote")
-	public JsonNode getPlanQuote(@RequestBody JsonNode jsonNode) {
-		return insuranceClient.getPlanQuote(jsonNode);
+	@GetMapping("findCustomerByUserId/{userId}")
+	public JsonNode findCustomerByUserId(@PathVariable Long userId) {
+		return insuranceClient.findCustomerByUserId(userId);
+	}
+
+	@PostMapping("addCustomerDocs")
+	public JsonNode addCustomerDocs(@PathVariable Long customerId, @PathVariable String addressDoc, @PathVariable String govIdDoc) {
+		return insuranceClient.addCustomerDocs(customerId, addressDoc, govIdDoc);
+	}
+	
+	//----------------- POLICY -----------------
+	@PostMapping("savePolicy")
+	public JsonNode savePolicy(@RequestBody JsonNode jsonNode) {
+		return insuranceClient.savePolicy(jsonNode);
+	}
+	
+	@GetMapping("findPolicyById/{policyId}")
+	public JsonNode findPolicyById(@PathVariable Long policyId) {
+		return insuranceClient.findPolicyById(policyId);
+	}
+	
+	@GetMapping("findAllPolicies")
+	public JsonNode findAllPolicies() {
+		return insuranceClient.findAllPolicies();
+	} 
+	
+	@GetMapping("findPolicyByStatus/{status}")
+	public JsonNode findPolicyByStatus(@PathVariable String status) {
+		return insuranceClient.findPolicyByStatus(status);
+	}
+	
+	@GetMapping("approvePolicy/{policyId}")
+	public JsonNode approvePolicy(@PathVariable Long policyId) {
+		return insuranceClient.approvePolicy(policyId);
+	}
+	
+	@GetMapping("denyPolicy/{policyId}")
+	public JsonNode denyPolicy(@PathVariable Long policyId) {
+		return insuranceClient.denyPolicy(policyId);
+	}
+	
+	//----------------- PAYMENT -----------------
+	@PostMapping("savePayment")
+	public JsonNode savePayment(@RequestBody JsonNode jsonNode) {
+		return insuranceClient.savePayment(jsonNode);
 	}
 }
