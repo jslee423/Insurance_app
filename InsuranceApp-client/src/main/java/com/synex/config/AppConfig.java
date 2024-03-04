f33a14fc7b4490e6b27d91a910099a89d0f87609
Jason Lee <jslee423@gmail.com>
2024-03-04 13:28:42 -0500

    remove unsed imports

--- InsuranceApp-client/src/main/java/com/synex/config/AppConfig.java
+++ InsuranceApp-client/src/main/java/com/synex/config/AppConfig.java
@@ -8,10 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
